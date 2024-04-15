fbfe5b95d18e8ee39aaad97e39587866abe98132
lewin <57451950+LewinGerber@users.noreply.github.com>
2024-04-15 21:01:12 +0200

    make solution more generic

--- parser/parser.py
+++ parser/parser.py
@@ -1,11 +1,17 @@
+PROGRAM_TAPE_SEPARATOR = "111"
 STATE_SEPARATOR = "11"
 GENERAL_SEPARATOR = "1"
 
-input = "1010100010100110100101001001100010100001010011000100101001001100001010000101001100001001000010010011000010001001000100"
-tape = list("1001")
-states = {}
+whole_input = "10101000101001101001010010011000101000010100110001001010010011000010100001010011000010010000100100110000100010010001001111001"
 
-for line in input.split(STATE_SEPARATOR):
-    element = list(filter(None, line.split(GENERAL_SEPARATOR)))
-    states[str(len(element[0])) + str(len(element[1]))] = (len(element[2]), len(element[3]), len(element[4]))
+def parse(input: str):
+    program, tape = input.split(PROGRAM_TAPE_SEPARATOR, 1)
 
+    states = {}
+    for line in program.split(STATE_SEPARATOR):
+        element = list(filter(None, line.split(GENERAL_SEPARATOR)))
+        states[str(len(element[0])) + str(len(element[1]))] = (len(element[2]), len(element[3]), len(element[4]))
+    
+    return states, list(tape)
+
+print(parse(whole_input))
